cd3915c542c41168b0ce9945223305f8634a9717
MarkCuban <83606182+tenmark86@users.noreply.github.com>
2022-09-19 20:26:34 +0300

    update file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,43 @@
-# Installs a Nginx server
+# configure nginx server to listen to port 80
+# with permanent redirect for /redirect_me and handle
+# custom error 404 page
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+exec {'apt-get-update':
+  command => '/usr/bin/apt-get update'
+}
+
+package {'nginx':
+  ensure  => 'installed',
+  require => Exec['apt-get-update'],
+}
+
+file {'/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Holberton School',
+  require => Package['nginx']
+}
+
+file_line {'server_name _;':
+  path    => '/etc/nginx/sites-available/default',
+  line    => "\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\t}",
+  after   => 'server_name _;',
+  require => Package['nginx']
+}
+
+file {'/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page",
+  require => Package['nginx']
+}
+
+file_line {'server_name _; ':
+  path    => '/etc/nginx/sites-enabled/default',
+  line    => "\n\terror_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}",
+  after   => 'server_name _;',
+  require => Package['nginx']
+}
+
+service {'nginx':
+  ensure  => 'running',
+  require => Package['nginx']
 }
