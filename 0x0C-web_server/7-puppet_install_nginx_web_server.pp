f5e8feec7aa2219c77e37fbb00812c1ede38fcdf
MarkCuban <83606182+tenmark86@users.noreply.github.com>
2022-09-19 20:48:12 +0300

    update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,43 +1,79 @@
-# configure nginx server to listen to port 80
-# with permanent redirect for /redirect_me and handle
-# custom error 404 page
-
-exec {'apt-get-update':
-  command => '/usr/bin/apt-get update'
+# add stable version of nginx
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-package {'nginx':
-  ensure  => 'installed',
-  require => Exec['apt-get-update'],
+# update software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-file {'/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Holberton School',
-  require => Package['nginx']
+# install nginx
+package { 'nginx':
+  ensure     => 'installed',
 }
 
-file_line {'server_name _;':
-  path    => '/etc/nginx/sites-available/default',
-  line    => "\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\t}",
-  after   => 'server_name _;',
-  require => Package['nginx']
+# allow HTTP
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
 }
 
-file {'/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page",
-  require => Package['nginx']
+# change folder rights
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-file_line {'server_name _; ':
+# create index file
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+# create index file
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+# add redirection and error page
+file { 'Nginx default config file':
+  ensure  => file,
   path    => '/etc/nginx/sites-enabled/default',
-  line    => "\n\terror_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}",
-  after   => 'server_name _;',
-  require => Package['nginx']
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
+}
+",
+}
+# restart nginx
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 
-service {'nginx':
-  ensure  => 'running',
-  require => Package['nginx']
+# start service nginx
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
