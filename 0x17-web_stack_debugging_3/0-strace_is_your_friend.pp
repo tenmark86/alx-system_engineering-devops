12d923fa76a8fbbf262806959d9e3c9e7fcbc07f
tenmark86e <tenmark86@gmail.com>
2022-11-01 15:01:04 +0300

    0. Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,9 @@
+# Strace is your friend (fix wordpress) 
+exec { 'fix-wordpress':
+  environment => ['DIR=/var/www/html/wp-settings.php',
+                  'OLD=phpp',
+                  'NEW=php'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
+}
